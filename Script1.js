f72d4a79abc6193861afb5411a2e1852001546b2
arpansingh44 <46692891+arpansingh44@users.noreply.github.com>
2019-01-14 16:59:03 -0500

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+//NAME:Gurarpan Singh
+//date:jan 14,2019
+// Student:C0697553
+//Course CSD 2214  section 6
+
+
+let countdown;
+let timeinterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.queryselector('clock');
+const daysspan = clock.queryselector('.days');
+const hoursspan = clock.queryselector('.hours');
+const daysspan = clock.queryselector('.minutes');
+const daysspan = clock.queryselector('.seconds');
+
+
